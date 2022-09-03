a86f2a5485eaae2a7f1255269572988cb3d0858b
ADMIN <ADMIN@LAPTOP-7OOPAIV7>
2022-09-03 18:36:50 +0530

    this upload by tharun @0309

---
+++ Tharun/src/org/login/BrowserLaunch.java
@@ -0,0 +1,14 @@
+package org.login;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserLaunch {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\ADMIN\\eclipse-workspace\\Tharun\\Driver\\chromedriver.exe");
+WebDriver driver = new ChromeDriver();
+
+	}
+
+}
